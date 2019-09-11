87f40172c7b4d9610bfc6bae3268409b2d7de159
Ross Wintle <ross@oikos.org.uk>
2019-09-11 22:53:18 +0100

    Fixing up IE

--- chatbot.js
+++ chatbot.js
@@ -99,6 +99,15 @@ var chat = {
 	}
 };
 
+/* NodeList.forEach polyfill */
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
 
 const bot = function () {
 
